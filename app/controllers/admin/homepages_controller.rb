71bb4d50ea3c06a4ce0293169286f38291e8fb71
Maverlous <quang1958vinh@gmail.com>
2020-09-06 14:52:06 +0700

    admin

--- app/controllers/admin/homepages_controller.rb
+++ app/controllers/admin/homepages_controller.rb
@@ -1,4 +1,12 @@
 class Admin::HomepagesController < Admin::BaseController
+  before_action :check_if_admin
   def index
   end
+
+  private
+  def check_if_admin 
+    redirect_to root_path unless current_user.admin?
+  end
+
+
 end
